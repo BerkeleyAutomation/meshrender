d60459621482ccd8074e3178668c73b6f43e942a
Matthew Matl <matthewcmatl@gmail.com>
2018-02-21 16:39:22 -0800

    Update viewer arguments

--- meshrender/constants.py
+++ meshrender/constants.py
@@ -1,3 +1,5 @@
 Z_NEAR = 0.05     # Near clipping plane, in meters
 Z_FAR = 100.0     # Far clipping plane, in meters
 MAX_N_LIGHTS = 10 # Maximum number of lights allowed
+OPEN_GL_MAJOR = 3 # Target OpenGL Major Version
+OPEN_GL_MINOR = 2 # Target OpenGL Minor Version
